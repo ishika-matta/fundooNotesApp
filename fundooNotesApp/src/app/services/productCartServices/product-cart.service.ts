a39d1f4dd26f95526ce05ef7315842fa5ed338b4
ishika-matta <imatta09@gmail.com>
2019-11-06 19:29:06 +0530

    50

---
+++ fundooNotesApp/src/app/services/productCartServices/product-cart.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from '../httpServices/http.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductCartService {
+  url: any;
+  auth = false;
+
+  constructor(private httpSvc: HttpService) { }
+
+  cartId() {
+    this.url = 'productcarts/myCart';
+    this.auth = true;
+    return this.httpSvc.getCall(this.url, this.auth);
+  }
+
+  placeOrder(data) {
+    this.url = 'productcarts/placeOrder';
+    this.auth = true;
+    return this.httpSvc.postCall(data, this.url, this.auth);
+  }
+}
