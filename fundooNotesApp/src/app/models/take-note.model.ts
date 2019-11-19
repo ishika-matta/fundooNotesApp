f83df8d42633b2df828ba02427899fe02ea47473
ishika-matta <imatta09@gmail.com>
2019-11-19 15:16:19 +0530

    .

--- fundooNotesApp/src/app/models/take-note.model.ts
+++ fundooNotesApp/src/app/models/take-note.model.ts
@@ -1,13 +1,10 @@
 export class TakeNote {
     title?: String;
     description?: 'string';
-    // isPined:"boolean";
-    // isArchived: "boolean";
     isDeleted?: Boolean;
     color?: 'string';
     id?: String;
     reminder?:any;
-    // userId: "string";
     service?: String;
     label?: String;
     labelIdList?:any;
